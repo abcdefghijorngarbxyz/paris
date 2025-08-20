168a6cbee515ff1c5e764a6ed5a3ad7f83cc0f01
dfntlynotjorn <dfntlynotjorn@gmail.com>
2025-08-21 01:32:22 +0800

    utc+8

--- src/timestamp/mod.rs
+++ src/timestamp/mod.rs
@@ -5,17 +5,28 @@ pub fn now() -> String {
     let since_epoch = current
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
-    let timestamp = since_epoch.as_secs();
+
+    let mut timestamp = since_epoch.as_secs();
+
+    // apply UTC+8 (8 hours * 3600 seconds)
+    timestamp += 8 * 3600;
 
     let hours = (timestamp % 86400) / 3600;
     let minutes = (timestamp % 3600) / 60;
     let seconds = timestamp % 60;
 
-    let is_pm = hours > 12;
+    let is_pm = hours >= 12;
+    let display_hour = if hours == 0 {
+        12
+    } else if hours > 12 {
+        hours - 12
+    } else {
+        hours
+    };
 
     let stamp = format!(
         "<dimmed>{:02}:{:02}:{:02} {}: </>",
-        hours,
+        display_hour,
         minutes,
         seconds,
         if is_pm { "PM" } else { "AM" }
